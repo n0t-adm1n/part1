788e83aa4a336de512f67fa387478e1078d3f24e
N0tAdm1n <ahmed29shamir@gmail.com>
2026-01-18 14:50:32 +0530

    Done exercise 1.7

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -15,24 +15,82 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
+
 
   //event handlers
+  const handleGoodClick = () => {
+    
+    let newGood = good + 1;
+    setGood(newGood);
+    
+    let newAll = all + 1;
+    setAll(newAll);
+
+    console.log(good, bad, neutral)
+
+    setAverage((newGood - bad) / newAll);
+    setPositive(newGood / newAll );
+  }
+
+  const handleNeutralClick = () => {
+    
+    let newNeutral = neutral + 1;
+    setNeutral(newNeutral);
+    
+    let newAll = all + 1;
+    setAll(newAll);
+
+    console.log(good, bad, neutral)
+
+    setAverage((good - bad) / newAll);
+    setPositive(good / newAll );
+  }
+
+  const handleBadClick = () => {
+    
+    let newBad = bad + 1;
+    setBad(newBad);
+    
+    let newAll = all + 1;
+    setAll(newAll);
+
+    console.log(good, newBad, neutral)
+
+    setAverage((good - newBad) / newAll);
+    setPositive(good / newAll );
+  }
+
   const handleClick = (setter, state) => {
-    return ()=>{setter(state + 1)}
+    return ()=>{
+      setter(state + 1);
+      let newAll = all + 1;
+      setAll(newAll);
+
+      console.log(good, bad, neutral)
+
+      setAverage((good - bad) / newAll);
+      setPositive(good / newAll );
+    }
   }
 
 
   return (
     <>
       <Heading text={"give feedback"} />
-      <Button onClick={handleClick(setGood, good)} text={"good"} />
-      <Button onClick={handleClick(setNeutral, neutral)} text={"neutral"} />
-      <Button onClick={handleClick(setBad, bad)} text={"bad"} />
+      <Button onClick={handleGoodClick} text={"good"} />
+      <Button onClick={handleNeutralClick} text={"neutral"} />
+      <Button onClick={handleBadClick} text={"bad"} />
 
       <Heading text={"statistics"} />
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>posivtive {positive} %</p>
     </>
   )
 }

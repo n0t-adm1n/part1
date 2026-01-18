fafe3cc57c7f05397762e21c3f21c530d4217d3c
N0tAdm1n <ahmed29shamir@gmail.com>
2026-01-18 15:53:15 +0530

    Done exercise 1.14

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -33,12 +33,30 @@ const App = () => {
     setVotes(copy)
   }
 
+  const getMostVotedAnecdote = () => {
+    let max = 0;
+    let maxIndex = 0;
+    votes.forEach((vote, idx) => {
+      if(vote > max) 
+      {
+        max = vote;
+        maxIndex = idx;
+      }
+    })
+
+    return anecdotes[maxIndex]
+  }
+
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
       <button onClick={handleVote}>vote</button>
       <button onClick={handleNext}>next anecdote</button>
+
+      <h1>Anecdote with most votes</h1>
+      <p>{getMostVotedAnecdote()}</p>
     </div>
   )
 }
